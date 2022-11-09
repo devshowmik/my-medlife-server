a5f3975da7743c12bf78ce640b3ec1f9aae1e258
devshowmik <devshowmik@gmail.com>
2022-11-09 13:26:49 +0600

    Setup server

---
+++ index.js
@@ -0,0 +1,56 @@
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express');
+const app = express();
+const corse = require('cors');
+app.use(corse());
+app.use(express.json());
+
+const Port = process.env.Port || 5000;
+// Check Server port on server console
+app.listen(Port, () => {
+    console.log('server is running on', Port)
+})
+// check server is running on cliend side
+app.get('/', (req, res) => {
+    res.send('Server is runnig')
+})
+// database
+//user : vector-photography
+// password : hTMFqF7N65YXUWRe
+
+
+const uri = "mongodb+srv://vector-photography:hTMFqF7N65YXUWRe@users.95k0mgf.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        // Create blog collection
+        const blogCollection = client.db('vector-photography').collection('blog');
+        // Send data to Database
+        app.post('/blogs', async (req, res) => {
+            const post = req.body;
+            const result = await blogCollection.insertOne(post);
+            res.send(result);
+        })
+        // get data from database and send it to client side
+        app.get('/blogs', async (req, res) => {
+            const query = {};
+            const cursor = blogCollection.find(query);
+            const blog = await cursor.toArray();
+            res.send(blog)
+
+        })
+        // delete from database
+        app.delete('/blogs/:postId', async (req, res) => {
+            const id = req.params.postId;
+            console.log('trying to delete', id)
+            const query = { _id: ObjectId(id) }
+            const deletedPost = await blogCollection.deleteOne(query);
+            res.send(deletedPost);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
